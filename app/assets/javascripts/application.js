640617b97f9faaf712db21eb53b1ed3c3b298459
Prashant Tiwari <prashaantt@gmail.com>
2015-07-21 19:28:15 +0530

    More layout improvements

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,8 +12,9 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
-//= require bootstrap-sprockets
+//= require bootstrap/transition
+//= require bootstrap/carousel
 //= require color-thief
 //= require jquery.color
+//= require turbolinks
 //= require_tree .
--- app/assets/javascripts/flowers.js
+++ app/assets/javascripts/flowers.js
@@ -1,17 +1,44 @@
-var getImageColor = function () {
-  var mainImage = $('.main-img');
-  if (mainImage.length > 0) {
-    var colorThief = new ColorThief();
-    setTimeout(function() {
-      try {
-        var color = colorThief.getColor(mainImage[0]);
-        $('body').animate({
-          backgroundColor: 'rgba(' + color.toString() + ', 0.7) !important'
-        }, 750);
-      } catch (e) {}
-    }, 200);
-  }
-}
+(function() {
+  var attrs = {
+    mainImg: '.main-img',
+    variantImg: '.variant-img',
+    flowersCarousel: '#flowers-carousel'
+  };
 
-$(document).ready(getImageColor);
-$(document).on('page:load', getImageColor);
+  var ready = function () {
+    get$([attrs.mainImg, attrs.variantImg]).on('load', function(){
+      blendBackgroundColour(this);
+    });
+
+    get$(attrs.flowersCarousel).on('slide.bs.carousel', function (event) {
+      blendBackgroundColour(event.relatedTarget.children[0]);
+    });
+  }
+
+  var blendBackgroundColour = function (img) {
+    try {
+      var color = new ColorThief().getColor(img);
+      $('body').animate({
+        backgroundColor: 'rgb(' + color.toString() + ')'
+      }, 1500);
+    } catch (e) {}
+  }
+
+  var get$ = function (selector) {
+    if (typeof selector == 'string') {
+      return $(selector);
+    } else if (selector instanceof Array) {
+      var selectors = '';
+      selector.forEach(function (sel, i) {
+        if (i > 0) {
+          selectors += ', '
+        }
+        selectors += sel;
+      });
+      return $(selectors);
+    }
+  }
+
+  $(document).ready(ready);
+  $(document).on('page:load', ready);
+}).call(this);
