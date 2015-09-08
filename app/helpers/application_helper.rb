2ca985b1f7b5784ef6e175e0df4625c82779e189
Deepika Tiwari <deepikatiwari92@gmail.com>
2015-09-08 14:34:26 +0530

    Add Google Analytics
    Change “Flowers” to “Flower Song”

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 	# Provides the title for every page.
 	def full_title(page_title = '')
-		base_title = "Flowers"
+		base_title = "Flower Song"
 		if page_title.empty?
 			base_title
 		else
